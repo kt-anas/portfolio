c13c799020b0a417502799a3cb009dbd96f9ea5f
anas <ktmanas2020@gmail.com>
2024-11-04 17:59:18 +0530

    routs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
  
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/home'
+import About from './pages/about'
+import Works from './pages/works'
+import Contact from './pages/contact'
+import Layout from './components/Layout'
 
 function App() {
-   
 
   return (
-    <>
-      
-    </>
+     <BrowserRouter>
+       <Routes>
+          <Route  path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route  path="/about" element={<About />} />
+          <Route path='/works' element={<Works />} />
+          <Route path='/contact' element={<Contact />} />
+          </Route>
+       </Routes>
+     </BrowserRouter>
   )
 }
 
