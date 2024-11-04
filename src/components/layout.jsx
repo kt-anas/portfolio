cb22bda470412569bf5aa011b381a8ee66dd8c2a
anas <ktmanas2020@gmail.com>
2024-11-04 19:28:47 +0530

    nabar

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from './Navbar'
+import Navbar from './navBar'
 import styles from '../styles/components/layout.module.css'
 // import Footer from './footer'
 
