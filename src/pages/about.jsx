21d07c37b4fd9722760fa22929fde391b7cd972b
anas <ktmanas2020@gmail.com>
2024-11-20 22:13:41 +0530

    blur

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/pages/about.module.scss';
 
 function About() {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={`${styles.absolute} ${styles['justify-center']}`}>
+        <div className={`${styles.bgShape} ${styles.bgTealGreen} ${styles.bgBlur}`} />
+        <div className={`${styles.bgShape} ${styles.bgTealBlue} ${styles.bgBlur}`} />
+        <div className={`${styles.bgShape} ${styles.bgTealRed} ${styles.bgBlur}`} />
+      </div>
       <h1>About</h1>
-      
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
